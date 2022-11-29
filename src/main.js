ade8756419c5b69902519ac65f8162c5ea30266d
Eric Kong <erickongdata@gmail.com>
2022-11-29 11:42:58 +0000

    Add grid-small for topic1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './styles/index.scss';
+
+const title6 = document.querySelectorAll(
+  '[data-id="headline"] .card-small__topic'
+)[1];
+
+title6.style.color = '#00b2ff';
+console.log(title6);
